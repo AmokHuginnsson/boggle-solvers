8c1d6bd52251684757059234e5b82b1130d726d6
Marcin Konarski <marcin.konarski@codestation.org>
2016-02-06 04:16:23 +0100

    Initial commit of boggle-solvers project.

---
+++ solve.php
@@ -0,0 +1,146 @@
+#! /bin/sh
+#<?php /*
+exec php7.0 "${0}" "${@}"
+#*/
+
+function array_get( $dict, $key, $default = null ) {
+	return ( array_key_exists( $key, $dict ) ? $dict[$key] : $default );
+}
+
+class DictionaryNode {
+	public $_finished = false;
+	public $_next = array();
+
+	function add_suffix( $suffix_ ) {
+		if ( strlen( $suffix_ ) == 0 ) {
+			$this->_finished = true;
+		} else {
+			$char = $suffix_[0];
+			if ( ! array_key_exists( $char, $this->_next ) ) {
+				$this->_next[$char] = new DictionaryNode();
+			}
+			$this->_next[$char]->add_suffix( substr( $suffix_, 1 ) );
+		}
+	}
+
+	function get( $char_ ) {
+		return ( array_get( $this->_next, $char_, null ) );
+	}
+
+	function print_all( $prefix_ = "" ) {
+		if ( $this->_finished ) {
+			print( $prefix_ );
+		}
+		foreach ( $this->_next as $char => $tail ) {
+			$tail->print_all( $prefix_ . $char );
+		}
+	}
+}
+
+class Cube {
+	public $_letter = null;
+	public $_visited = false;
+	public $_neighbors = [];
+	function __construct( $letter_ ) {
+		$this->_letter = $letter_;
+	}
+}
+
+class Board {
+	public $_cubes = null;
+	public $_size = 0;
+	function __construct( $letters_ ) {
+		$len = intval( sqrt( floatval( strlen( $letters_ ) ) ) );
+		$len2 = $len * $len;
+		assert( $len2 == strlen( $letters_ ) );
+		$this->_size = $len;
+		$this->_cubes = [];
+		for ( $i = 0; $i < $len2; ++ $i ) {
+			array_push( $this->_cubes, new Cube( $letters_[$i] ) );
+		}
+		$deltas = [[-1,-1], [-1,0], [-1,1], [0,-1], [0,1], [1,-1], [1,0], [1,1]];
+		for ( $x = 0; $x < $len; ++ $x ) {
+			for ( $y = 0; $y < $len; ++ $y ) {
+				foreach ( $deltas as $d ) {
+					$nx = $x + $d[0];
+					$ny = $y + $d[1];
+					if ( ( $nx >= 0 ) && ( $nx < $len ) && ( $ny >= 0 ) && ( $ny < $len ) ) {
+						array_push( $this->get_cube( $x, $y )->_neighbors, $this->get_cube( $nx, $ny ) );
+					}
+				}
+			}
+		}
+	}
+
+	function get_cube( $x_, $y_ ) {
+		return ( $this->_cubes[$y_ * $this->_size + $x_] );
+	}
+
+	function solve( $dictionary_ ) {
+		$result = [];
+		foreach ( $this->_cubes as $cube ) {
+			$this->solve_recursive( $result, "", $cube, $dictionary_ );
+		}
+		$result = array_unique( $result );
+		usort(
+			$result,
+			function( $a, $b ) {
+				return ( strlen( $a ) - strlen( $b ) );
+			}
+		);
+		return ( $result );
+	}
+
+	function solve_recursive( &$result, $prefix_, $cube_, $dictNode_ ) {
+		$nextNode = $dictNode_->get( $cube_->_letter );
+		if ( $nextNode == null ) {
+			return;
+		}
+		$cube_->_visited = true;
+		$newPrefix = $prefix_ . $cube_->_letter;
+		if ( $nextNode->_finished && ( strlen( $newPrefix ) >= 3 ) ) {
+			array_push( $result, $newPrefix );
+		}
+		foreach ( $cube_->_neighbors as $neighbor ) {
+			if ( ! $neighbor->_visited ) {
+				$this->solve_recursive( $result, $newPrefix, $neighbor, $nextNode );
+			}
+		}
+		$cube_->_visited = false;
+	}
+}
+
+function main( $argv ) {
+	$dictionaryRoot = new DictionaryNode();
+	$dictFile = fopen( $argv[1], "r" );
+	while ( ( $line = fgets( $dictFile ) ) !== false ) {
+		$dictionaryRoot->add_suffix( trim( $line ) );
+	}
+	fclose( $dictFile );
+	print( "[ OK ] Ready\n" );
+	$letters = "";
+	while ( true ) {
+		$line = fgets( STDIN );
+		if ( strlen( $line ) == 0 ) {
+			break;
+		}
+		$line = trim( $line );
+		if ( ( strlen( $letters ) > 0 ) && ( strlen( $line ) == 0 ) ) {
+			print( "[ OK ] Solving\n" );
+			$board = new Board( $letters );
+			foreach ( $board->solve( $dictionaryRoot ) as $word ) {
+				print( "(" . strlen( $word ) . ") " . $word . "\n" );
+			}
+			print( "[ OK ] Solved\n" );
+			$letters = "";
+		} else {
+			$letters = $letters . $line;
+		}
+	}
+	return ( 0 );
+}
+
+main( $argv );
+
+?>
+
---
+++ solve.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python2
+
+from __future__ import print_function
+from __future__ import division
+import codecs
+import math
+import sys
+import traceback
+
+
+class DictionaryNode(object):
+	__slots__ = ['finished', '_next']
+
+	def __init__(self):
+		self.finished = False
+		self._next = {}
+
+	def add_suffix(self, suffix):
+		if not suffix:
+			self.finished = True
+		else:
+			char = suffix[0]
+			if not char in self._next:
+				self._next[char] = DictionaryNode()
+			self._next[char].add_suffix(suffix[1:])
+
+	def get(self, char):
+		return self._next.get(char, None)
+
+	def print_all(self, prefix=""):
+		if self.finished:
+			print(prefix)
+		for char in self._next:
+			self._next[char].print_all(prefix + char)
+
+
+class Board(object):
+	class Cube(object):
+		def __init__(self, letter):
+			self.letter = letter
+			self.visited = False
+			self.neighbors = []
+
+	def __init__(self, letters):
+		size = int(math.sqrt(len(letters)))
+		assert size * size == len(letters)
+		self._size = size
+		self._cubes = [self.Cube(l) for l in letters]
+		deltas = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+		for x in range(size):
+			for y in range(size):
+				for (dx, dy) in deltas:
+					nx, ny = x + dx, y + dy
+					if nx >= 0 and nx < size and ny >= 0 and ny < size:
+						self.get_cube(x, y).neighbors.append(self.get_cube(nx, ny))
+
+	def get_cube(self, x, y):
+		return self._cubes[y * self._size + x]
+
+	def solve(self, dictionary):
+		result = set()
+		for cube in self._cubes:
+			self._solve_recursive(result, "", cube, dictionary)
+		return sorted(result, key=len)
+
+	def _solve_recursive(self, result, prefix, cube, dict_node):
+		next_node = dict_node.get(cube.letter)
+		if next_node is None:
+			return
+		cube.visited = True
+		new_prefix = prefix + cube.letter
+		if next_node.finished and len(new_prefix) >= 3:
+			result.add(new_prefix)
+		for neighbor in cube.neighbors:
+			if not neighbor.visited:
+				self._solve_recursive(result, new_prefix, neighbor, next_node)
+		cube.visited = False
+
+
+def main():
+	dictionary_root = DictionaryNode()
+	with codecs.open(sys.argv[1], encoding="latin2") as dict_file:
+		for line in dict_file:
+			dictionary_root.add_suffix(line.strip())
+	print('[ OK ] Ready', file=sys.stderr)
+	letters = ''
+	while True:
+		line = sys.stdin.readline()
+		if not line:
+			break
+		line = line.strip()
+		if letters and not line:
+			print('[ OK ] Solving', file=sys.stderr)
+			try:
+				board = Board(unicode(letters, encoding='utf-8'))
+				for word in board.solve(dictionary_root):
+					print(u'({}) {}'.format(len(word), word))
+				print('[ OK ] Solved', file=sys.stderr)
+			except Exception as e:
+				print('[FAIL] {}:\n{}'.format(e, traceback.format_exc()))
+			letters = ''
+		else:
+			letters = letters + line
+
+
+if __name__ == '__main__':
+	main()
