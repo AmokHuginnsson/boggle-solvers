7d22ee3ee2c91c5f9cc93dc4d2cf765ad7b5af84
Marcin Konarski <marcin.konarski@codestation.org>
2016-02-08 20:07:03 +0100

    js: Drop support for rhino as it is too old and lacks support for "for..of".

--- solve.js
+++ solve.js
@@ -2,67 +2,44 @@
 //bin/true \
 /*
 exec nodejs --harmony "${0}" "${@}"
-exec rhino -version 170 "${0}" "${0}" "${@}"
-Very ugly hack to spawn nodejs/rhino from ${PATH}.
+Very ugly hack to spawn nodejs from ${PATH}.
 */
 
-/* initalize for node.js/rhino compatibility */
-var ARGV = arguments
-if ( typeof ( print ) === "undefined" ) {
-	print = console.log
-}
-var openFile = null
-var readLine = null
-var stdIn = null
-if ( typeof ( process ) !== "undefined" ) { /* nodejs */
-	ARGV = process.argv.splice( 1 )
+print = console.log
+var ARGV = process.argv.splice( 1 )
 //	var iconv = require( "iconv" ).Iconv
-	var fs = require( "fs" )
-	var bufferSize = 1024
-	openFile = function( path_ ) {
-		return ( { fd: fs.openSync( path_, "r" ), buffer: new Buffer( bufferSize ), leftOver: "" } )
+var fs = require( "fs" )
+var bufferSize = 1024
+function openFile( path_ ) {
+	return ( { fd: fs.openSync( path_, "r" ), buffer: new Buffer( bufferSize ), leftOver: "" } )
+}
+function stdIn() {
+	if ( this.fd === undefined ) {
+		this.fd = openFile( "/dev/stdin" )
 	}
-	stdIn = function() {
-		if ( this.fd === undefined ) {
-			this.fd = openFile( "/dev/stdin" )
+	return ( this.fd )
+}
+function readLine( fd ) {
+	if ( fd.leftOver === null ) {
+		fd.leftOver = ""
+	}
+	while ( true ) {
+		var read = 0
+		var idx = 0
+		if ( ( idx = fd.leftOver.indexOf( "\n", 0 ) ) !== -1 ) {
+			var line = fd.leftOver.substring( 0, idx )
+			fd.leftOver = fd.leftOver.substring( idx + 1 )
+			return ( line )
+		} else if ( ( read = fs.readSync( fd.fd, fd.buffer, 0, bufferSize, null ) ) === 0 ) {
+			break;
+		} else if ( read > 0 ) {
+			fd.leftOver += fd.buffer.toString( undefined, 0, read )
 		}
-		return ( this.fd )
 	}
-	readLine = function( fd ) {
-		if ( fd.leftOver === null ) {
-			fd.leftOver = ""
-		}
-		while ( true ) {
-			var read = 0
-			var idx = 0
-			if ( ( idx = fd.leftOver.indexOf( "\n", 0 ) ) !== -1 ) {
-				var line = fd.leftOver.substring( 0, idx )
-				fd.leftOver = fd.leftOver.substring( idx + 1 )
-				return ( line )
-			} else if ( ( read = fs.readSync( fd.fd, fd.buffer, 0, bufferSize, null ) ) === 0 ) {
-				break;
-			} else if ( read > 0 ) {
-				fd.leftOver += fd.buffer.toString( undefined, 0, read )
-			}
-		}
-		if ( fd.leftOver.length == 0 ) {
-			fd.leftOver = null
-		}
-		return ( fd.leftOver )
+	if ( fd.leftOver.length == 0 ) {
+		fd.leftOver = null
 	}
-} else { /* rhino */
-	openFile = function( path_ ) {
-		return ( new java.io.BufferedReader( new java.io.FileReader( path_ ) ) )
-	}
-	stdIn = function() {
-		if ( this.fd === undefined ) {
-			this.fd = new java.io.BufferedReader( new java.io.InputStreamReader( java.lang.System.in ) )
-		}
-		return ( this.fd )
-	}
-	readLine = function( fd ) {
-		return ( fd.readLine() )
-	}
+	return ( fd.leftOver )
 }
 
 function DictionaryNode() {
